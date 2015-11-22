5bd187e09822ae387b48ee2f01d85131b3f5b70c
cckeating <conner_rocks157@hotmail.com>
2015-11-22 14:22:39 -0500

    updated maps.h

--- include/riotMap.h
+++ include/riotMap.h
@@ -39,109 +39,63 @@ struct TileNode {/*
 };
 
 
-/* Function Prototypes */
-
-void parseMap(char *loadDir, struct MapList* mapList, struct Dialog dialog[]);/*
-
-DESCRIPTION: parseMap() will parse level files and store their contents into
- the structures used by the rest of the system.
-
-ARGUMENTS: either a custom path containing the desired map files, or NULL in
- which case the default map directory will be sourced.
-
-POSTCONDITIONS: A Map struct is returned containing the map elements and
- tower placements.*/
+/*DESCRIPTION: Parse level files and store their contents into
+  the structures used by the rest of the system.
+  ARGUMENTS: -The directory where the maps are found (char *loadDir).
+             -List of maps being added (struct MapList* mapList)
+             -Sets the dialog for each map (struct Dialog dialog[]).
+  POSTCONDITIONS: A Map struct is returned containing the map elements and
+  tower placements.*/
+void parseMap(char *loadDir, struct MapList* mapList, struct Dialog dialog[]);
 
 
-
-void getPath(struct Path* path, struct Map map);/*
-
-DESCRIPTION: Retreieves information from the given map and stores this data into a path structure.
-             Returns said path structure.
-
-ARGUMENTS: The map that will be parsed.*/
+/*DESCRIPTION: Retrieves information from the given map and stores this data into a path structure.
+  ARGUMENTS: -The path to be populated (struct Path *path).
+             -Map being parsed (struct Map map).*/
+void getPath(struct Path *path, struct Map map);
 
 
+/*DESCRIPTION: Recursive function which creates an ordered path struct.
+  ARGUMENTS: -Map struct to be searched (struct Map map).
+             -Path struct to be created(struct Path *path).
+             -Previously checked positions(int prevChecked[]).
+             -Number of the tile that has been added to the path(int count).
+             -Current position being searched in the map struct(int currentPosition). */
+struct Path *pathSolve(struct Map map, struct Path *path, int prevChecked[], int count, int currentPosition);
 
 
-
-void addToPath(struct Path *path, int location, char type);/*
-
-DESCRIPTION: Adds an additional tile to the path
-
-ARGUMENTS: The path which the tile is being added to
-           The location(position) of where the tile will be located
-           The type of tile that is going to be added*/
+/*DESCRIPTION: Pushes a tile node to the back of the path list.
+  ARGUMENTS: -Tile Node to be pushed (struct TileNode* insertNode).
+             -Path that the tile node is pushed into (struct Path* path).*/
+void pushToPath(struct TileNode* insertNode, struct Path* path);
 
 
-struct UnitList* getGuards(struct UnitList *guards, struct Map map);/*
-
-DESCRIPTION: Retrieves the guard data from the given map and stores the guards into a linked list UnitList structure.
-
-ARGUMENTS: The map that will be parsed */
-
-
-bool isPathCharacter(char tileChar);/*
-
-DESCRIPTION: Returns a boolean based on whether the character is a path character.
-
-ARGUMENTS: Character to be checked.
-*/
+/*DESCRIPTION: Checks whether the position exists in the previously checked array.
+  ARGUMENTS: -Array of all positions previously checked (int prevChecked[]).
+             -Position to be compared against the previously checked positions (int position).*/
+bool beenChecked(int prevChecked[], int position);
 
 
-struct TileNode * createTileNode(int location, char type);/*
-
-DESCRIPTION: Creates a tile node and initializes it.
-
-ARGUMENTS: Location of the tile node (int location).
-           Character type of the tile node (char type).
-*/
+/*DESCRIPTION: Returns a boolean based on whether the character is a path character.
+  ARGUMENTS: -Character to be checked (char tileChar).*/
+bool isPathCharacter(char tileChar);
 
 
-void destroyPath(struct Path * path);/*
-
-DESCRIPTION: Destroys the path and all the contents of the path.
-
-ARGUMENTS: Path to be destroyed (struct Path *path).
-*/
+/*DESCRIPTION: Creates a tile node and initializes it.
+  ARGUMENTS: -Location of the tile node (int location).
+             -Character type of the tile node (char type).*/
+struct TileNode *createTileNode(int location, char type);
 
 
-void pushToPath(struct TileNode* insertNode, struct Path* path);/*
-
-DESCRIPTION: Pushes a tile node to the back of the path list.
-
-ARGUMENTS: Tile Node to be pushes (struct TileNode* insertNode).
-           Path that the tile node is pushed into (struct Path* path).
-*/
+/*DESCRIPTION: Destroys the path and all the contents of the path.
+  ARGUMENTS: -Path to be destroyed (struct Path *path).*/
+void destroyPath(struct Path *path);
 
 
-struct Path *pathSolve(struct Map map,struct Path *path,int prevChecked[],int count,int currentPosition);/*
+/*DESCRIPTION: Copies the elements from the initalMap to the dupeMap
+  ARGUMENTS: -Initial Map Pointer, Map pointer for the duplicate to be used during gameplay (struct Map *initialMap).
+             -Duplicate map being created (struct Map *dupeMap).*/
+void copyMap (struct Map *initialMap, struct Map *dupeMap);
 
-DESCRIPTION: Recursive function which creates an ordered path struct.
-
-ARGUMENTS: Map struct to be searched (struct Map map).
-           Path struct to be created(struct Path *path).
-           Previously checked positions(int prevChecked[]).
-           Number of the tile that has been added to the path(int count).
-           Current position being searched in the map struct(int currentPosition).
-
-*/
-
-
-bool beenChecked(int prevChecked[],int position);/*
-
-DESCRIPTION: Checks whether the position exists in the previously checked array.
-
-ARGUMENTS: Array of all positions previously checked (int prevChecked[]).
-           Position to be compared against the previously checked positions.
-*/
-
-
-void copyMap (struct Map *initialMap, struct Map *dupeMap);/*
-
-DESCRIPTION: Copies the elements from the initalMap to the dupeMap
-
-ARGUEMENTS: Initial Map Pointer, Map pointer for the duplicate to be used during gameplay
-*/
 
 #endif //RIOT_MAP
